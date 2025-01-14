f47238287ba55cb01abda8d1582bab94f1c3ab67
suzuki hironobu <hironobu@interdb.jp>
2025-01-14 12:44:11 +0900

    Update copyright for 2025

--- pg_crash.c
+++ pg_crash.c
@@ -2,7 +2,7 @@
  * pg_crash.c
  *
  * Author: suzuki hironobu (hironobu@interdb.jp) 7, Dec, 2020
- * Copyright (C) 2020-2024  suzuki hironobu
+ * Copyright (C) 2020-2025  suzuki hironobu
  *-------------------------------------------------------------------------
  */
 #include "postgres.h"
